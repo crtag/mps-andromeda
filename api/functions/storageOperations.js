934bd0e70a5a06a9b6de8df1d7355d628d941b9b
Alexey Novikov <former.token@gmail.com>
2025-12-09 19:28:52 +1300

    Minor changes to enable local development with remote storage bucket

--- api/functions/storageOperations.js
+++ api/functions/storageOperations.js
@@ -15,7 +15,20 @@ const RESULTS_PREFIX = "job-results/";
 const TRAJECTORY_PREFIX = "job-trajectories/";
 
 // Get default bucket reference
-const getBucket = () => storage.bucket();
+const getBucket = () => {
+    const red = '\x1b[31m';
+    const green = '\x1b[32m';
+    const reset = '\x1b[0m'; // Resets color to default
+
+    if ('true' === process.env.FUNCTIONS_EMULATOR) {
+        console.log(green + "Using dev storage bucket: mps-andromeda-dev.appspot.com" + reset);
+        // dealing with the dev environment
+        const bucketName = "mps-andromeda-dev";
+        return storage.bucket(bucketName);
+    }
+    console.log(red + "Using production storage bucket" + reset);
+    return storage.bucket()
+};
 
 // Utility function to list files with a specific prefix
 async function listFilesWithQuery(prefix, matchGlob) {
